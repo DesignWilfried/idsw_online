edb036bd8fd0416ac94e7bd82b0fb1a4e70a0725
DesignWilfried <166217914+DesignWilfried@users.noreply.github.com>
2025-11-18 11:13:43 +0100

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,11 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores([
+      "node_modules/", 
+      "build/",
+      "dist/"
+    ]),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
